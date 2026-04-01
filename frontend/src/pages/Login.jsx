75c2f7a7acdfd2e9dc31741d79e6916cd3eed8a4
Софья <sonkild@yandex.ru>
2026-04-01 17:58:02 +0300

    feat: добавить валидацию email и скрытие пароля

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { TextField, Button, Container, Typography, Box, Paper, Alert } from '@mui/material';
+import { TextField, Button, Container, Typography, Box, Paper, Alert, InputAdornment, IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { authApi } from '../api/index';
@@ -7,15 +8,21 @@ import { authApi } from '../api/index';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState('');
 
     // Фунция логина из контекста
     const { login } = useAuth();
     const navigate = useNavigate();
 
+    const isEmailValid = (email) => /\S+@\S+\.\S+/.test(email);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setError('');
+        if (!isEmailValid(email)) {
+            setError('Введите корректный Email');
+            return
+        }
 
         try {
             // Запрос
@@ -23,7 +30,7 @@ const Login = () => {
 
             // Ответ
             const { accessToken, refreshToken } = response.data;
-
+            
             // Обновление глобального состояния
             login(accessToken, refreshToken);
             navigate('/');
@@ -51,6 +58,8 @@ const Login = () => {
                         label='Email'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        error={email !== '' && !isEmailValid(email)}
+                        helperText={email !== '' && !isEmailValid(email) ? "Неверный формат почты" : ""}
                         />
 
                         <TextField
@@ -58,8 +67,18 @@ const Login = () => {
                         fullWidth
                         required
                         label='Пароль'
+                        type={showPassword ? 'text' : 'password'}
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                        {showPassword ? <VisibilityOff /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
                         />
 
                         <Button
